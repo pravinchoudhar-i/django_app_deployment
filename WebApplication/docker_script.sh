1a007c0843b2e3d0a770b91c384e699110dffbf8
pravinchoudhar-i <pravinchoudhari237@gmail.com>
2024-06-10 15:13:25 +0530

    Docker file added

---
+++ WebApplication/docker_script.sh
@@ -0,0 +1,17 @@
+echo "Enter tag name: "
+read docker_tag
+
+echo "Enter container name: "
+read container_name
+
+echo "Enter server port: "
+read server_port
+
+echo "Enter docker port: "
+read docker_port
+
+
+sudo docker build -t $container_name .
+sudo docker stop $docker_tag
+sudo docker run -d --rm --name $docker_tag -p$server_port:$docker_port $container_name
+
